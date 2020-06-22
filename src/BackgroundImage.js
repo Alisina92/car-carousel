06744babe72fd8bc618a773a8f761ce7c049256c
[Alisina92] <asb92.jobs@gmail.com>
2020-06-22 21:11:24 +0100

    Alisina-Image-carousel-proj-react 1st Component done

---
+++ src/BackgroundImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import image from "./image/IBDlhN.jpg";
+
+const BackgroundImage =()=>{
+    return(<div>
+        <img className ='img' src ={image} alt='East German flag'/>
+         
+    </div>)
+}
+
+export default BackgroundImage;
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title =()=>{
+   return(<div>
+       <h1 className="h1">Eastern Bloc Car Show</h1>;
+    </div>)
+   
+}
+
+export default Title;
